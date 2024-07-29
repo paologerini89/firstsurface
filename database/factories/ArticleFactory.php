4572b82c71651b2f60ced12e1b44db6b7019f146
Paolo Gerini <paolo.gerini@isendu.com>
2024-07-29 16:48:10 +0200

    init with article

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->company(),
+            'content' => fake()->realText(),
+            'type' => fake()->randomElement(['news', 'blog', 'press']),
+            'author' => fake()->name()
+        ];
+    }
+}
